0b71824d4f23202373df24b38c35ea14e1111716
Aleksandr Klimenkov <prisioner@mail.ru>
2019-04-08 20:04:49 +0300

    max multiplication

--- lib/itransition_test_task.rb
+++ lib/itransition_test_task.rb
@@ -1,3 +1,16 @@
 module ItransitionTestTask
   extend self
+
+  def max_multiplication(str)
+    return unless str.is_a?(String)
+
+    candidates = str.scan(/(?=(\d{4}))/)
+
+    return unless candidates.any?
+
+    candidates
+      .flatten
+      .map { |digit| digit.split("").map(&:to_i).inject(&:*) }
+      .max
+  end
 end
--- spec/itransition_test_task_spec.rb
+++ spec/itransition_test_task_spec.rb
@@ -1,4 +1,48 @@
 require "itransition_test_task"
 
 RSpec.describe ItransitionTestTask do
+  describe "max_multiplication" do
+    subject { described_class.max_multiplication(input_string) }
+
+    context "when not-a-string passed" do
+      let(:input_string) { 12345 }
+      it "returns nil" do
+        expect(subject).to be_nil
+      end
+    end
+
+    context "when string passed" do
+      context "when string doesn't contain 4 digits in a row" do
+        let(:input_string) { "a1b2c3d4e" }
+
+        it "returns nil" do
+          expect(subject).to be_nil
+        end
+      end
+
+      context "when max multiplication in beginning of digits row" do
+        let(:input_string) { "abc45231def" }
+
+        it "returns max multiplication" do
+          expect(subject).to eq 120
+        end
+      end
+
+      context "when max multiplication in the end of digits row" do
+        let(:input_string) { "abc12345def" }
+
+        it "returns max multiplication" do
+          expect(subject).to eq 120
+        end
+      end
+
+      context "when max multiplication in the middle of digits row" do
+        let(:input_string) { "abc123451def" }
+
+        it "returns max multiplication" do
+          expect(subject).to eq 120
+        end
+      end
+    end
+  end
 end
